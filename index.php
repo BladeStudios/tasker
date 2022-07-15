9afbea2011f277d8470bb53a7a621319498ca915
Blade Studios <maciejwarakomski@gmail.com>
2022-07-15 09:04:15 +0200

    added simple div structure on main page and simple css

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <html>
-    <head>
-        <title>Tasker (Powered by BladeStudios (C) 2022)</title>
-    </head>
-    <body>
-        IN PROGRESS. STAY TUNED.
-    </body>
+<head>
+    <title>Tasker (Powered by BladeStudios (C) 2022)</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="top">
+    <div id="app_title">TASKER</div>
+    <div id="app_desc">Online Task Manager</div>
+</div>
+<div id="menu">
+    <span class="menu_link">link 1</span>
+    <span class="menu_link">link 2</span>
+    <span class="menu_link">link 3</span>
+</div>
+<div id="container"></div>
+<div id="footer">Powered by BladeStudios (C) 2022</div>
+
+</body>
 </html>
