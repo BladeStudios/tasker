1b74d4d8f1989755b08e119f5e4c0b0fb45c62db
Blade Studios <maciejwarakomski@gmail.com>
2022-08-16 12:53:42 +0200

    added few columns to users table, changed table name from 'statuses' to 'online_statuses', added 'friendships' table and 'friendship_statuses' table

--- sql/patch_2022-07-15_1112.sql
+++ sql/patch_2022-07-15_1112.sql
@@ -5,8 +5,8 @@ CREATE TABLE `tasker`.`positions` (
     `name` varchar(32) NOT NULL COMMENT 'position name, like: Admin, Operator, Moderator, User etc.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `tasker`.`statuses` (
-    `id` int(11) DEFAULT NULL UNIQUE COMMENT 'online status; number refers to online status, ex. online, offline, AFK etc.',
+CREATE TABLE `tasker`.`online_statuses` (
+    `id` int(11) DEFAULT NULL UNIQUE COMMENT 'online status ID',
     `name` varchar(32) NOT NULL COMMENT 'status name, like: online, offline, AFK etc.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -23,7 +23,26 @@ CREATE TABLE `tasker`.`users` (
   `last_activity` DATETIME DEFAULT NULL COMMENT 'date and time when the user was last seen as online or active',
   `browser` varchar(32) DEFAULT NULL COMMENT 'name of the browser that user uses',
   `system` varchar(32) DEFAULT NULL COMMENT 'name of the Operating System that user uses',
+  `create_time` DATETIME DEFAULT NULL COMMENT 'date and time when the user created his account',
+  `banned_until` DATETIME DEFAULT NULL COMMENT 'date and time when the account will be unbanned',
+  `level` int(11) NOT NULL COMMENT 'current account level (depends on experience points)',
+  `experience` int(11) NOT NULL COMMENT 'current number of experience points for doing tasks'
     PRIMARY KEY (id),
     FOREIGN KEY (`position`) REFERENCES positions(id),
-    FOREIGN KEY (`status`) REFERENCES statuses(id)
+    FOREIGN KEY (`status`) REFERENCES online_statuses(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tasker`.`friendship_statuses` (
+    `id` int(11) DEFAULT NULL UNIQUE COMMENT 'friendship status ID',
+    `name` varchar(32) NOT NULL COMMENT 'status name, like: PENDING, ACCEPTED, REJECTED, BLOCKED etc.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tasker`.`friendships` (
+    `id` int(11) DEFAULT NULL UNIQUE COMMENT 'friendship ID',
+    `inviter_id` int(11) NOT NULL COMMENT 'ID of user who invited the another user to the friendlist',
+    `invitee_id` int(11) NOT NULL COMMENT 'ID of user who was invited by another user to the friendlist',
+    `status_id` int(11) NOT NULL COMMENT 'friendship status ID',
+    FOREIGN KEY ('inviter_id') REFERENCES users(id),
+    FOREIGN KEY ('invitee_id') REFERENCES users(id),
+    FOREIGN KEY ('status_id') REFERENCES friendship_statuses(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
