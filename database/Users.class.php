47dbe6fffbf3b30b7410a5e2cd1334f0cffed199
Blade Studios <maciejwarakomski@gmail.com>
2022-08-17 16:34:02 +0200

    added missing columns (online_from and total_online_time) to addUser function in User.class.php

--- database/Users.class.php
+++ database/Users.class.php
@@ -12,9 +12,9 @@ class User
         $conn = $db_obj->connect();
 
         $sql = "INSERT INTO ".$this->tableName."
-        (login, password, salt, email, position_id, status_id, current_ip, last_ip, last_activity, browser, system, create_time,
+        (login, password, salt, email, position_id, status_id, last_ip, current_ip, online_from, total_online_time, last_activity, browser, system, create_time,
         banned_until, level, experience, timezone_id)
-        VALUES (:login, :password, :salt, :email, :position_id, :status_id, :current_ip, :last_ip, :last_activity, :browser, :system, :create_time,
+        VALUES (:login, :password, :salt, :email, :position_id, :status_id, :last_ip, :current_ip, :online_from, :total_online_time, :last_activity, :browser, :system, :create_time,
         :banned_until, :level, :experience, :timezone_id)";
 
         $st = $conn->prepare($sql);
@@ -24,8 +24,10 @@ class User
         $st->bindParam('email',$email);
         $st->bindParam('position_id',$position_id);
         $st->bindParam('status_id',$status_id);
-        $st->bindParam('current_ip',$current_ip);
         $st->bindParam('last_ip',$last_ip);
+        $st->bindParam('current_ip',$current_ip);
+        $st->bindParam('online_from',$online_from);
+        $st->bindParam('total_online_time',$total_online_time);
         $st->bindParam('last_activity',$last_activity);
         $st->bindParam('browser',$browser);
         $st->bindParam('system',$system);
@@ -40,14 +42,14 @@ class User
         $status_id = 0;
         $current_ip = $ip;
         $last_ip = $ip;
+        $online_from = null;
+        $total_online_time = 0;
         $last_activity = $time;
         $create_time = $time;
         $banned_until = null;
         $level = 1;
         $experience = 0;
         $timezone_id = 1;
-
-        
 
         $st->execute();
         $db_obj->disconnect();
@@ -104,6 +106,66 @@ class User
             echo "Error: ".$e->getMessage();
             return -1;
         }  
+    }
+
+    public function getUserByLogin($login)
+    {
+        require_once('database/Database.class.php');
+
+        try
+        {
+            $db_obj = new Database();
+            $conn = $db_obj->connect();
+
+            $sql = "SELECT * FROM users WHERE login=:login";
+
+            $st = $conn->prepare($sql);
+            //$st->bindParam('login',$login);
+            $data = [
+                'login' => $login
+            ];
+
+            $st->execute($data);
+            $st->setFetchMode(PDO::FETCH_ASSOC);
+            $db_obj->disconnect();
+            return $st->fetchAll();
+        }
+        catch(PDOException $e)
+        {
+            echo "Error: ".$e->getMessage();
+            return -1;
+        }  
+    }
+
+    public function updateUser($last_ip, $current_ip)
+    {
+        require_once('database/Database.class.php');
+
+        try
+        {
+            $db_obj = new Database();
+            $conn = $db_obj->connect();
+
+            $sql = "UPDATE users SET last_ip";
+
+            $st = $conn->prepare($sql);
+            //$st->bindParam('login',$login);
+            $data = [
+                'login' => $login
+            ];
+
+            $st->execute($data);
+            $st->setFetchMode(PDO::FETCH_ASSOC);
+            $db_obj->disconnect();
+            return $st->fetchAll();
+        }
+        catch(PDOException $e)
+        {
+            echo "Error: ".$e->getMessage();
+            return -1;
+        } 
+
+        $sql = "UPDATE users SET last_ip=$previous_ip, cur_ip='$ip', online=1, online_from='$timestamp', last_activity='$timestamp', system='$user_os', browser='$user_browser' WHERE id='$id'";
     }
 }
 
