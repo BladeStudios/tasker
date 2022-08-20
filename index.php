2fcda04abdb86b2f61f7a621cb9a9bbeb0584365
Blade Studios <maciejwarakomski@gmail.com>
2022-08-20 23:59:11 +0200

    created a simple, almost fully working task view (some small fixes still needed, like css and making START/PAUSE/FINISH buttons work)

--- index.php
+++ index.php
@@ -31,23 +31,80 @@
 
     if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']===true)
     {
-        //
         if(isset($_SESSION['id']))
         {
+            require_once('database/Database.class.php');
+            $database = new Database();
+            if(!$database->doesUserExist($_SESSION['id']))
+            {
+                unset($_SESSION['loggedin']);
+                unset($_SESSION['id']);
+                $_SESSION['error'] = $lang['e_account_permissions'];
+                exit(header('Location: login.php'));
+            }
+
+            $ok = true; //flag to check if all data from the database was received successfully
+
+            require_once('database/User.class.php');
+            $user_obj = new User();
+
             require_once('database/Task.class.php');
             $task_obj = new Task();
-            $tasklist = $task_obj->getTaskListForUser($_SESSION['id']);
-            foreach($tasklist as $task)
+            $taskList = $task_obj->getTaskListForUser($_SESSION['id']);
+            if($taskList===false) $ok = false;
+
+            $tableNames = array(
+                'task_difficulties',
+                'task_statuses',
+                'task_types',
+                'task_visibilities'
+            );
+
+            $taskInfo = array();
+
+            foreach($tableNames as $tableName)
             {
+                $taskInfo[$tableName] = array();
+                $data = $database->getTable($tableName);
+                if(!$data) $ok = false;
+                foreach($data as $val)
+                {
+                    $taskInfo[$tableName][$val['id']] = $val['name'];
+                }
+            }
+
+            if(!$ok)
+            {
+                $_SESSION['error'] = $lang['e_database'];
+                if(isset($_SESSION['error']))
+                {
+                    echo '<div style="color: red; font-family: Verdana; font-size: 15pt">'.$_SESSION['error'].'</div>';
+                    unset($_SESSION['error']);
+                }
+                exit;
+            }
+
+            foreach($taskList as $task)
+            {
+                $createdBy = $user_obj->getUserById($task['creator_id'])['login'];
+
+                switch($taskInfo['task_difficulties'][$task['difficulty_id']])
+                {
+                    case 'Easy': $difficultyColor = 'green'; break;
+                    case 'Medium': $difficultyColor = 'yellow'; break;
+                    case 'Hard': $difficultyColor = 'red'; break;
+                    default: $difficultyColor = 'black';
+                }
+
+                if(empty($task['deadline'])) $task['deadline'] = 'none';
+
                 echo '
                 <div class="task" data-user-id="'.$task['id'].'">
                     <div class="task-name-row">
-                        <span class="task-name">taskname</span>
-                        <span class="task-status">TODO</span>
+                        <span class="task-name">'.$task['name'].'</span>
+                        <span class="task-status">'.$taskInfo['task_statuses'][$task['status_id']].'</span>
                     </div>
-                    <div class="task-description">
-                        description description description description description description description description description description description description description description description description description description description description
-                    </div>
+                    <div class="task-description">'.$task['description'].'</div>
                     <div class="task-info-table-container">
                         <table class="task-info-table">
                             <tr>
@@ -58,11 +115,11 @@
                                 <td>XP earned</td>
                             </tr>
                             <tr>
-                                <td class="task-deadline">t</td>
-                                <td class="task-difficulty">t</td>
-                                <td class="task-time-spent">t</td>
-                                <td class="task-value">t</td>
-                                <td class="task-xp-earned">t</td>
+                                <td class="task-deadline">'.$task['deadline'].'</td>
+                                <td class="task-difficulty" style="font-weight: bold; color: '.$difficultyColor.'">'.$taskInfo['task_difficulties'][$task['difficulty_id']].'</td>
+                                <td class="task-time-spent">'.$task['time_spent'].'</td>
+                                <td class="task-value">'.$task['time_exp'].' XP/min</td>
+                                <td class="task-xp-earned">'.$task['total_exp'].'</td>
                             </tr>
                         </table>
                     </div>
@@ -76,11 +133,11 @@
                     </div>
                 </div>
                 <div class="task-more-details">
-                    <div class="task-details-added">sa</div>
-                    <div class="task-details-created-by">das</div>
-                    <div class="task-details-assigned-to">das</div>
-                    <div class="task-details-visibility">dasd</div>
-                    <div class="task-details-minimum-xp">das</div>
+                    <div class="task-details-added">Added: '.$task['created'].'</div>
+                    <div class="task-details-created-by">Created by: '.$createdBy.'</div>
+                    <div class="task-details-assigned-to">Assigned to: '.$_SESSION['login'].'</div>
+                    <div class="task-details-visibility">Visibility: '.$taskInfo['task_visibilities'][$task['visibility_id']].'</div>
+                    <div class="task-details-minimum-xp">Minimum XP for this task: '.$task['base_exp'].'</div>
                 </div>
                 ';
             }
@@ -100,6 +157,16 @@
 ?>
 </div>
 <?php require('footer.php'); ?>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.btnTaskStart').click(function(){
+            taskElement = $(this).parent().parent();
+            taskId = taskElement.data('user-id');
+            alert('task id=' + taskElement.data('user-id'));
+        })
+    });
+</script>
 
 </body>
 </html>
